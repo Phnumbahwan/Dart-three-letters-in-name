15137a76a0abc14ad6eed0c68b442ab9641e1a60
Gil lopez <52746589+Phnumbahwan@users.noreply.github.com>
2020-09-16 13:40:08 +0800

    Create gil.dart

---
+++ gil.dart
@@ -0,0 +1,236 @@
+void main() {
+  String name = 'BA E';
+  Name name_three_letters = Name(name);
+  name_three_letters.display();
+}
+
+class Name {
+  String name;
+  
+  Name (String name) {
+    this.name = name;
+  }
+  
+  void display() {
+    try{
+      int size = 3;
+      for(int x = 0;x < size;x++){
+        switch(this.name[x].toUpperCase()){
+          case ' ': 
+            size++;
+            break;
+          case 'A': 
+            print('     *');
+            print('    *  *');
+            print('   *    *');
+            print('  ********');
+            print(' *        *');
+            print('*          *');
+            break;
+          case 'B': 
+            print('*******');
+            print('*      *');
+            print('* ****');
+            print('*     *');
+            print('*      *');
+            print('*******');
+            break;
+          case 'C': 
+            print(' ******');
+            print('*');
+            print('*');
+            print('*');
+            print('*');
+            print(' ******');
+            break;
+          case 'D': 
+            print('*****');
+            print('*    *');
+            print('*     *');
+            print('*     *');
+            print('*    *');
+            print('*****');
+            break;
+          case 'E': 
+            print('********');
+            print('*');
+            print('*******');
+            print('*');
+            print('*');
+            print('********');
+            break;
+          case 'F': 
+            print('******');
+            print('*');
+            print('****');
+            print('*');
+            print('*');
+            print('*');
+            break;
+          case 'G': 
+            print(' *****');
+            print('*');
+            print('*');
+            print('*   ***');
+            print('*     *');
+            print('******');
+            break;
+          case 'H': 
+            print('*     *');
+            print('*     *');
+            print('*******');
+            print('*     *');
+            print('*     *');
+            print('*     *');
+            break;
+          case 'I': 
+            print('*****');
+            print('  *');
+            print('  *');
+            print('  *');
+            print('  *');
+            print('*****');
+            break;
+          case 'J': 
+            print('*******');
+            print('   *');
+            print('   *');
+            print('*  *');
+            print('*  *');
+            print('***');
+            break;
+          case 'K': 
+            print('*   *');
+            print('*  *');
+            print('* *');
+            print('*  *');
+            print('*   *');
+            print('*    *');
+            break;
+          case 'L': 
+            print('*');
+            print('*');
+            print('*');
+            print('*');
+            print('*');
+            print('******');
+            break;
+          case 'M': 
+            print('*     *');
+            print('* * * *');
+            print('*  *  *');
+            print('*     *');
+            print('*     *');
+            print('*     *');
+            break;
+          case 'N': 
+            print('*      *');
+            print('* *    *');
+            print('*  *   *');
+            print('*   *  *');
+            print('*    * *');
+            print('*      *');
+            break;
+          case 'O': 
+            print('  *****');
+            print(' *     *');
+            print('*       *');
+            print('*       *');
+            print(' *     *');
+            print('  *****');
+            break;
+          case 'P': 
+            print('****');
+            print('*   *');
+            print('*   *');
+            print('* *');
+            print('*');
+            print('*');
+            break;
+          case 'O': 
+            print('  *****');
+            print(' *     *');
+            print('*       *');
+            print('*     * *');
+            print(' *     *');
+            print('  *****  *');
+            break;
+          case 'P': 
+            print('****');
+            print('*   *');
+            print('*   *');
+            print('* *');
+            print('*  *');
+            print('*    *');
+            break;
+          case 'S': 
+            print(' *****');
+            print('*     *');
+            print(' *');
+            print('   *');
+            print('*    *');
+            print(' ****');
+            break;
+          case 'T': 
+            print('********');
+            print('   *');
+            print('   *');
+            print('   *');
+            print('   *');
+            print('   *');
+            break;
+          case 'U': 
+            print('*     *');
+            print('*     *');
+            print('*     *');
+            print('*     *');
+            print('*     *');
+            print(' ****');
+            break;
+          case 'V': 
+            print('*         *');
+            print(' *       *');
+            print('  *     *');
+            print('   *   *');
+            print('    * *');
+            print('     *');
+            break;
+          case 'W': 
+            print('*       *');
+            print('*   *   *');
+            print('*   *   *');
+            print('*   *   *');
+            print('*   *   *');
+            print(' **  **');
+            break;
+          case 'X': 
+            print('*     *');
+            print(' *   *');
+            print('  * * ');
+            print('   *');
+            print(' *   *');
+            print('*     *');
+            break;
+          case 'Y': 
+            print('*     *');
+            print(' *   *');
+            print('  **');
+            print('  *');
+            print(' *');
+            print('*');
+            break;
+          case 'Z': 
+            print('*******');
+            print('     *');
+            print('   *');
+            print('  *');
+            print(' *');
+            print('*******');
+            break;
+        }
+      }
+    }catch(e){
+      print('Warning : Make atleast 3 letters word');
+    }
+  }
+}
